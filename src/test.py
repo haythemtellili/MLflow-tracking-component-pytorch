7968aedf5cc30c6e32a535ffb4d15330953557bd
Haythem Tellili <haythemtellili@Haythems-MacBook-Pro.local>
2021-12-03 11:52:53 +0100

    bug fixed on test.py

--- src/test.py
+++ src/test.py
@@ -13,21 +13,17 @@ def test(args, model, loader_test):
     model.eval()
     fn_list = []
     pred_list = []
-    for x, fn in loader_test:
+    targets = []
+    for x, fn, label in loader_test:
         with torch.no_grad():
             x = x.float()
             x = x.to(args.device)
-            output = model(x)
+            output = best_model(x)
             pred = torch.argmax(output, dim=1)
             fn_list += [n[:-4] for n in fn]
             pred_list += [p.item() for p in pred]
-    sub = pd.DataFrame({"id": fn_list, "pred": pred_list})
-    sub["target"] = -1
-    for i in range(len(sub["id"])):
-        if "wp_correct" in sub["id"][i]:
-            sub["target"][i] = 0
-        else:
-            sub["target"][i] = 1
+            targets += [p.item() for p in label]
+    sub = pd.DataFrame({"id":fn_list, "pred":pred_list, "target": targets})
     # Evaluate (simple)
     metrics = precision_recall_fscore_support(
         sub["target"], sub["pred"], average="weighted"
